2b1c01945739ba9dcc8a67a8def3dd62f98834f7
lastbencher147 <lastbencher147@gmail.com>
2012-03-21 12:13:52 +0000

    Added code for member screen

--- trunk/src/test/java/unit/org/nthdimenzion/presentation/infrastructure/NavigationTest.java
+++ trunk/src/test/java/unit/org/nthdimenzion/presentation/infrastructure/NavigationTest.java
@@ -2,6 +2,8 @@ package org.nthdimenzion.presentation.infrastructure;
 
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.zkoss.zk.ui.Execution;
 
 import java.util.Collections;
@@ -9,10 +11,12 @@ import java.util.Collections;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+@ActiveProfiles({"standalone"})
 public class NavigationTest {
 
     @Test
-    public void testRedirectUrl(){
+    public void testRedirectUrl() {
         Execution execution = mock(Execution.class);
         ResourceBundleViewResolver resolver = mock(ResourceBundleViewResolver.class);
         Navigation navigation = new Navigation(resolver);
@@ -25,13 +29,13 @@ public class NavigationTest {
 
 
     @Test
-    public void testRedirectUrlWithParams(){
+    public void testRedirectUrlWithParams() {
         Execution execution = mock(Execution.class);
         ResourceBundleViewResolver resolver = mock(ResourceBundleViewResolver.class);
         Navigation navigation = new Navigation(resolver);
         when(resolver.resolveViewName("test")).thenReturn("test.zul");
 
-        String url = navigation.buildUrl("test", ImmutableMap.of("id","2","name","sud"));
+        String url = navigation.buildUrl("test", ImmutableMap.of("id", "2", "name", "sud"));
 
         assertTrue("test.zul?id=2&name=sud".equals(url));
     }
